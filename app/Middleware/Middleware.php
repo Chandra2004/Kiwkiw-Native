8065ebb4401c3974a151888204e18fc95fb3679f
Chandra Tri Antomo <88928844+Chandra2004@users.noreply.github.com>
2025-03-15 14:03:03 +0700

    Delete app directory

--- app/Middleware/Middleware.php
+++
@@ -1,7 +0,0 @@
-<?php
-    namespace User\KiwkiwNative\Middleware;
-
-    interface Middleware {
-        function before();
-    }
-?>
--- app/helpers.php
+++
@@ -1,9 +0,0 @@
-<?php
-    use User\KiwkiwNative\App\Config;
-
-    if (!function_exists('url')) {
-        function url($path = '') {
-            return rtrim(Config::get('BASE_URL'), '/') . '/' . ltrim($path, '/');
-        }
-    }
-?>
